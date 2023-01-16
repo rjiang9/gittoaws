e0c723c5dfc23b1e199f1077f02b39b2826abf52
Ray <umjcl@msn.com>
2023-01-16 12:19:44 -0700

    1st

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Ray is test out Amplify Git deplop CPCI.
         </a>
       </header>
     </div>
